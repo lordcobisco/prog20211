11f94fc068003583a978d46bdde1fca82ae36826
SZALLA <souvenir.zalla@edu.isd.org.br>
2021-08-24 20:19:01 -0300

    PseudoCódigo_Exercício

---
+++ PseudoCódigo_Exercício.py
@@ -0,0 +1,23 @@
+"PseudoCódigo_Exercício"
+# Início
+# Definir horário de saída (B);
+# Leia (B) = 7:00hs;
+# Ler horário no relógio” (A);
+#      Se A for > ou = B, então:
+#          Ir para o ponto de ônibus;
+#          Aguardar próximo ônibus
+#          Verificar letreiro de itinerário de cada ônibus;
+#                        Se letreiro do ônibus for o de destino para o ISD, então:
+#                                      Sinalizar ao motorista uma parada;
+#                                      Tomar o ônibus;
+#                                      No ônibus verificar a próxima parada;
+#                                               Se a parada for o ISD, então:
+#                                                         Descer no ponto;
+#                                                         Dirigir-se a sala de aula.
+#                                                         Finalizar
+#                                               Senão, permanecer no ônibus;
+#                         Senão: 
+#                         Aguardar próximo ônibus; 
+#                         Verificar letreiro de itinerário de casa ônibus;
+#     Senão
+#      Ler horário no relógio” (A);
