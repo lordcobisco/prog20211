cae725331e7953f23ff883577e2add10f1acce78
pedrosaleslc <88834552+pedrosaleslc@users.noreply.github.com>
2021-08-24 22:27:54 -0300

    atividades aulas 1, 2 e 3

---
+++ AtividadeIMC.py
@@ -0,0 +1,15 @@
+#Exercício IMC 1
+
+altura = float(1.65)
+peso = float(65)
+imc = peso/(altura*altura)
+print(imc)
+
+
+#Exercício IMC 2
+
+altura = float(input('Entre com sua altura:'))  
+peso = float(input('Entre com seu peso:'))  
+imc = peso/(altura * altura)  
+print('Seu índice de massa corporal é {:.2f}'.format(imc))  
+
