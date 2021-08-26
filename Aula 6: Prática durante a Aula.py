226f2d21f502bc1572153d4416480724d2e36a42
ftthiagoaraujo <88913003+ftthiagoaraujo@users.noreply.github.com>
2021-08-26 09:24:38 -0300

    Update Aula 6: Prática durante a Aula.py

--- Aula 6: Prática durante a Aula.py
+++ Aula 6: Prática durante a Aula.py
@@ -1,19 +1,11 @@
-string
-#casa vermeha
+#QUESTÕES OBJETIVAS
+#Questão 1: Quais os principais tipos de de estruturas de repetição?
+#Questão 2: Escolha 3 tipos de dados compostos e aplique em 3 exemplos de estruturas de repetição.
+#Questão 3: Crie 2 exemplos de estruturas de repetição em problemas práticos e relacionados a projetos de neuroengenharia.
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+#PERGUNTAS DAS AUALS
+# Questão 1: Quais aplicações podem ser enxergadas para estruturas de repetição?
+# Resposta 1: 
 
 
 
