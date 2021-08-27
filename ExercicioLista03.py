5970f3de44804c000092eae1717412d8fd8894f1
Kellisson Kayonário <37579620+kyonario@users.noreply.github.com>
2021-08-26 23:14:43 -0300

    conclusão código ExercícioLista03 e mudança do documento Resposta trabalho 1 principal

--- ExercicioLista03.py
+++ ExercicioLista03.py
@@ -1,17 +1,12 @@
 
-#resImagemAlt
-#resImagemLar
-
+#Kelisson Lima
 # Para públicação em periódicos Isto significa que é mais seguro
 #coletar imagens para publicação com formatos de 1024 x 1024 ou até 2048 x 2048
 
 resImagemAlt = 768 
 resImagemLar = 1366
 tipoCelulaAmostra = "osteoblastos"
-#40 vezes
-posicaoObjetivaCima = 40
-posicaoObjetivaBaixo = 40
-# Choose “speed” or “slow” buttons to switch between coarse and fine focus 
+# Escolha “speed” or “slow” que irá ajustar o foco fino ou grosso 
 ajusteFoco = "speed"
 #Argon ou Halogen
 #Argon é um laser que mostra a fluorecencia da amostra
@@ -22,16 +17,14 @@ lampadaHalogenLigado = False
 #Bloqueador de lâmpada de mercúrio, pois a longa exposição à luz pode 
 # branquear suas amostras e mudar o filtro para 1
 bloquearLampada = False
-
-### 1 = cinza
-#2 = blue e.g. DAPI
-#3 = green e.g. FITC/SYTO 9
-#4 = red e.g. TRITC/RHOD/Texas Red
-#5 = to use as a ‘normal’ microscope with a halogen light source
-
-# Para Lamparada Halogen use the knob
-#to increase the light intensity. You can view your sample using the VIS button and filter set 5. 
+#1 = cinza
+#2 = blue 
+#3 = green 
+#4 = red 
+#5 = para usar como microscopio como ‘normal’  com a fonte de luz halogen 
 labelFiltro = 1
+# para aumentar a intensidade da luz .
+luzIntensidade = 5
 
 print('\n'*2)
 print('Esse programa tem como objetivo receber dados para o Software LSM usado em um microscópio confocal')
@@ -68,6 +61,10 @@ bloquearLampadaIn = int (input("| bloquearLampada | -*- [Sim-1/Não-0] Informe e
 infBloquearLampada = (bloquearLampada != bloquearLampadaIn)
 print ('Houve alteração na variável labelFiltro? ', infBloquearLampada)
 print("-----------------------------------------------------")
+luzIntensidadeIn = int (input("| luzIntensidade | [1/100] Informe em números: "))
+influzIntensidade = (luzIntensidade != luzIntensidadeIn)
+print ('Houve alteração na variável labelFiltro? ', influzIntensidade)
+print("-----------------------------------------------------")
 print ("\n" * 5)
 print('            INFORMAÇÕES SETADAS PELO USUÁRIO         ')
 print(' ______________________________________________________________')
@@ -77,46 +74,215 @@ print('|________________|__________________|_____________________ ')
 print('| Tipo Lâmpada   |  Ajuste do Foco  |  Filtro            ')
 print('|   %s                %s               %d                '%(tipoLampadaIn,ajusteFocoIn,labelFiltroIn))
 print('|________________|__________________|_____________________ ')
-print('| Bloq Lâmpada   | Nome Pesquisador |                      ')
-print('|   %d             %s                                     ' % (bloquearLampadaIn,nomePesquisador))
+print('| Bloq Lâmpada   | Nome Pesquisador | Luz Intensidade      ')
+print('|   %d             %s                 %d                    ' % (bloquearLampadaIn,nomePesquisador,luzIntensidadeIn))
 print('|________________|__________________|____________________________')
 print('\n'*4)
 print('            CALIBRAÇÃO DO EQUIPAMENTO         ')
 print(' ______________________________________________________________')
 print('\n'*2) 
-print('[!] Para calibrar o equipamento você precisa digitar')
-print('[!] a primeira letra do seu nome 10X e a ')
-print('[!] última letra do seu nome 10X também')
+print('[!] Para calibrar o equipamento você precisa executar os seguintes passos:')
+print('[ SENTIDO HORIZONTAL ]')
+print('[!] a PRIMEIRA letra do seu nome 10X e a ')
+print('[!] ÚLTIMA letra do seu nome 10X também')
 print('\n'*2)
-print('[!] Toque por 10X na tecla da primeira letra do seu primeiro nome')
+print('[ SENTIDO VERTICAL ]')
+print('[!] a SEGUNDA letra do seu nome 10X e a ')
+print('[!] PENÚLTIMA letra do seu nome 10X também')
+print('\n'*2)
 letraIni =nomePesquisador[0] #pega a letra incial
 comprimento = len(nomePesquisador) #comprimento do nome
-print(comprimento)
 letraFim = nomePesquisador[comprimento-1]
 print(letraFim)
-contador1=0
-contador2=0
+contador=0
+letraSeg = nomePesquisador[1]
+letraPen = nomePesquisador[comprimento-2]
 
-while (contador1 < 10):
+
+print('[HORIZONTAL] Toque por 10X na tecla da primeira letra do seu primeiro nome')
+while (contador < 10):
        entradaLetraIni = (input('Digite a letra | %s | = ' % letraIni))
        print('Digitou correto ? ', entradaLetraIni == letraIni)
-       contador1   = contador1 + int(entradaLetraIni == letraIni)
-print('[!] Toque por 10X na tecla da última letra do seu primeiro nome')
-while (contador2 < 10):
+       print('O que você digitou? ', entradaLetraIni)
+       contador   = contador + int(entradaLetraIni == letraIni)
+print('[HORIZONTAL] Toque por 10X na tecla da última letra do seu primeiro nome')
+contador = 0
+while (contador < 10):
        entradaLetraFim = (input('Digite a letra | %s | = ' % letraFim))
        print('Digitou correto ? ', entradaLetraFim == letraFim)
-       contador2   = contador2 + int(entradaLetraFim == letraFim)
+       print('O que você digitou? ', entradaLetraFim)
+       contador   = contador + int(entradaLetraFim == letraFim)
+print('[VERTICAL] Toque por 10X na tecla da segunda letra do seu primeiro nome')
+contador = 0
+while (contador < 10):
+       entradaLetraIni = (input('Digite a letra | %s | = ' % letraSeg))
+       print('Digitou correto ? ', entradaLetraIni == letraSeg)
+       print('O que você digitou? ', entradaLetraIni)
+       contador   = contador + int(entradaLetraIni == letraSeg)
+print('[VERTICAL] Toque por 10X na tecla da penúltima letra do seu primeiro nome')
+contador=0
+while (contador < 10):
+       entradaLetraFim = (input('Digite a letra | %s | = ' % letraPen))
+       print('Digitou correto ? ', entradaLetraFim == letraPen)
+       print('O que você digitou? ', entradaLetraFim)
+       contador   = contador + int(entradaLetraFim == letraPen)
+
+print('[<|>] ** EQUIPAMENTO CALIBRADO **')
 
 
 
 
 
 
-# posicaoObjetivaCimaIn  = int (input("| posicaoObjetivaCima  | -*- Informe o valor que deseja \
-# subir a objetiva: "))
-# print ('Houve alteração na variável posicaoObjetivaCimaIn? ', posicaoObjetivaCima != posicaoObjetivaCimaIn)
-# print("-----------------------------------------------------")
-# posicaoObjetivaBaixoIn  = int (input("| posicaoObjetivaBaixo  | -*- Informe o valor que deseja \
-# descer a objetiva: "))
-# print ('Houve alteração na variável posicaoObjetivaBaixo? ', posicaoObjetivaBaixo != posicaoObjetivaBaixoIn)
-# print("-----------------------------------------------------")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Kelisson Lima
