bb7901403eda330d4c7cb81ec0d27319fe4e3655
pedrosaleslc <88834552+pedrosaleslc@users.noreply.github.com>
2021-08-26 22:25:47 -0300

    Atualização atividade IMC
    Atualização da atividade de IMC referente a aula 4

--- AtividadeIMC.py
+++ AtividadeIMC.py
@@ -1,4 +1,4 @@
-#Exercício IMC 1
+
 
 altura = float(1.65)
 peso = float(65)
@@ -6,10 +6,15 @@ imc = peso/(altura*altura)
 print(imc)
 
 
-#Exercício IMC 2
-
 altura = float(input('Entre com sua altura:'))  
 peso = float(input('Entre com seu peso:'))  
 imc = peso/(altura * altura)  
 print('Seu índice de massa corporal é {:.2f}'.format(imc))  
 
+imc = 23.88
+if imc < 18:
+    print('você está abaixo do peso')
+if imc > 18 and imc < 25:
+    print('você está no peso ideal')
+if imc > 25:
+    print('você está acima do peso')             
