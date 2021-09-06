2926627b71c05d670376ecdede4c64e38fdb20f8
Raquel Emanuela de Medeiros <raquel.medeiros@campus-bib03.isd.matriz>
2021-09-06 16:49:02 -0300

    criaçao de arquivos aula 05

---
+++ Raquel/Aula05/Atividade05.py
@@ -0,0 +1,65 @@
+#Atividade Contextualizada 05
+
+# Questão 01 , alternativa A) 
+
+listaNome = ['regiao;estado;municipio;coduf;codmun;codRegiaoSaude;nomeRegiaoSaude;data;semanaEpi;populacaoTCU2019;casosAcumulado;casosNovos;obitosAcumulado;obitosNovos;Recuperadosnovos;emAcompanhamentoNovos;interior/metropolitana']
+lista = []
+lista = []
+lista = []
+lista = []
+lista = []
+lista = []
+lista = []
+lista = []
+lista = []
+lista = []
+lista = []
+lista = []
+lista = []
+lista = []
+lista = []
+lista = []
+lista = []
+lista = []
+lista = []
+lista = []
+lista = []
+lista = []
+lista = []
+lista = []
+lista = []
+lista = []
+lista = []
+lista = []
+lista = []
+lista = []
+lista = []
+lista = []
+lista = []
+lista = []
+
+
+
+print (lista[1][1])
+print('segundo dado da lista:')
+print (lista [1][1])
+print('')
+print('Total de casos do estado di Rio de Janeiro(tupla):')
+print ('primeiro dado da tupla:') 
+print (  [1][1])
+print('segundo dado da tupla:')
+print ( [1][1])
+
+
+
+
+print ('óbitos acumulados:' )
+print ('')
+print (lista [1][1]),':', (lista [1][1])
+
+
+
+
+
+
+
