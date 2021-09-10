b5285a94bf189d1496ba227ddb6dae139c4d9ae2
Souver <88834540+souvenirzalla@users.noreply.github.com>
2021-09-10 17:27:04 -0300

    Create operadores.py

---
+++ ZALLA_Exercícios_Aulas/operadores.py
@@ -0,0 +1,50 @@
+# Atribuição e operadores aritméticos
+
+resultadoSoma = 12+55 #Atribuição
+print(resultadoSoma)
+
+print(12+55) #Soma
+
+print(12-55) #Subtração
+
+print(10*55) #Multiplicação
+
+print(55/5) #Divisão
+
+print(12**2) #Exponenciação
+
+print(55//7) #Parte inteira da divisão
+
+print(55%7) #Resto da divisão
+
+# Operadores compostos
+
+maisIgual, menosIgual, vezesIgual, divididoIgual, moduloIgual = 20
+
+maisIgual += 4
+menosIgual -= 4
+vezesIgual *= 4
+divididoIgual /= 4
+moduloIgual %= 3
+
+x,y,z = 3,6,9
+x,y,z = x*3,x+y+z,x*y*z
+
+(a, b) = (2, 3)
+
+# Expressões lógicas e operadores relacionais
+
+varLogica = True
+print("Valor de 'not varLogica': ",not varLogica)
+
+varLogica1 = True
+varLogica2 = False
+print("Valor de 'varLogica1 e varLogica2: ",varLogica1 and varLogica2)
+print("Valor de 'varLogica1 ou varLogica2: ",varLogica1 or varLogica2)
+
+print(3==3)
+print(3!=3)
+print(3>3)
+print(3<3)
+print(3>=3)
+print(3<=3)
