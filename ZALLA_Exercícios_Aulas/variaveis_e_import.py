c94e04cbae1f8ac09bfa7bad8bcd7e533e38f75e
Souver <88834540+souvenirzalla@users.noreply.github.com>
2021-09-10 16:56:55 -0300

    Create variaveis_e_import.py

---
+++ ZALLA_Exercícios_Aulas/variaveis_e_import.py
@@ -0,0 +1,25 @@
+# Variáveis
+
+varString = "Souvenir Zalla"
+print(varString)
+print(type(varString))
+
+varInt = 25
+print(varInt)
+print(type(varInt))
+
+varFloat = 5.96
+print(varFloat)
+print(type(varFloat))
+
+varBool = True
+print(varBool)
+print(type(varBool))
+
+# Importando módulos
+
+import math
+print(math.sqrt(25))
+
+from math import sqrt
+print(sqrt(25))
