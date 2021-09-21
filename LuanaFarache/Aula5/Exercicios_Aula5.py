d74ce1302b42a1f0b5147fd76e9745b058e9b088
LuanaFarache <89166243+LuanaFarache@users.noreply.github.com>
2021-09-20 23:52:04 -0300

    Atividade5 - Iniciada

---
+++ LuanaFarache/Aula5/Exercicios_Aula5.py
@@ -0,0 +1,19 @@
+'''
+Exercícios da aula 5, referente a tipos compostos.
+'''
+# Exemplos de formas de declarar listas vazias:
+    #lista = []
+    #print(lista)
+
+    #lista = list()
+    #print(lista)
+
+# Exemplos de listas com elementos definidos:
+    #lista = ['Luana', 123, 1.5]
+    #print(lista)
+
+#Lista dentro de listas:
+lista = list(['Luana', 123, 1.5,['João', 'Maria', 2,20], 90])
+print(lista)
+
+
