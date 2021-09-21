e828eb026f6ac2453e8d3106b3788d37f2e2e3c9
Beatriz Moura <88495100+beamouraISD@users.noreply.github.com>
2021-09-21 11:24:59 -0300

    Update views.py

--- BeatrizMoura/aula13-BeatrizMoura/BokehDjango/BokehApp/views.py
+++ BeatrizMoura/aula13-BeatrizMoura/BokehDjango/BokehApp/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from bokeh.plotting import figure
+from bokeh.embed import components
+
+
 # Create your views here.
 
 def home(request):
+    plot = figure(plot_width = 400, plot_height = 400)
+    plot = circle([1,2,3,4,5], [6,7,8,9,1], plot_width = 400, plot_height = 400)
+
+    
     return HttpResponse('Isso aparece quando vc digita http://127.0.0.1:8000')
