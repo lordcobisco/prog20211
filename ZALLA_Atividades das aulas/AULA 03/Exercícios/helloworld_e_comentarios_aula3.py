5cbfe544f2291605fe16058b01d57706a9a6ca5e
Souver <88834540+souvenirzalla@users.noreply.github.com>
2021-10-06 16:21:42 -0300

    Delete ZALLA_Atividades das aulas directory

--- ZALLA_Atividades das aulas/AULA 03/Exercícios/helloworld_e_comentarios_aula3.py
+++
@@ -1,9 +0,0 @@
-# Nada que for colocado aqui aparecerá no programa
-# Nem será utilizado por ele
-
-print("Hello World!")
-
-'''
-Nada que for colocado aqui aparecerá no programa
-Nem será utilizado por ele
-'''
--- ZALLA_Atividades das aulas/AULA 03/Exercícios/input_e_print_aula3.py
+++
@@ -1,6 +0,0 @@
-num = input("Digite um número: ")
-print(num)
-
-login = input("Login: ")
-senha = input("Senha: ")
-print("O usuário informado foi: %s, e a senha digitada foi: %s." %(login, senha))
