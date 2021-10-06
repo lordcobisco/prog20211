f03ea87701749d0d65b3d19f054f39c8f8fc8b2e
Souver <88834540+souvenirzalla@users.noreply.github.com>
2021-10-06 14:47:17 -0300

    Add files via upload

---
+++ ZALLA_Aula_5/Exercícios/tuplas.py
@@ -0,0 +1,52 @@
+tupla = (1, 2, 3, 4)
+print(tupla)
+
+tupla = (1, )
+print(tupla)
+
+tupla = ()
+print(tupla)
+
+#####################
+
+tupla = ("Maria", "João", "Carlos")
+print(tupla[0])
+print(tupla[0:2])
+
+# tupla[0] = "Ana" (erro)
+
+####################
+
+p = "python"
+print(p[0:0])
+print(p[0:1])
+print(p[1:2])
+print(p[2:3])
+print(p[3:4])
+print(p[4:5])
+print(p[5:6])
+print(p[6:6])
+
+print(p[:1])
+print(p[:2])
+print(p[:3])
+print(p[:4])
+print(p[:5])
+print(p[:6])
+
+print(p[0:0])
+print(p[0:1])
+print(p[0:2])
+print(p[0:3])
+print(p[0:4])
+print(p[0:5])
+print(p[0:6])
+print(p[0:6])
+
+print(p[:])
+print(p[1:])
+print(p[2:])
+print(p[3:])
+print(p[4:])
+print(p[5:])
+print(p[6:])
