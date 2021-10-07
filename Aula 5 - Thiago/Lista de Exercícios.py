c90b77d185a716712f65b210c300e39a877d6032
ftthiagoaraujo <88913003+ftthiagoaraujo@users.noreply.github.com>
2021-10-07 10:17:14 -0300

    Atualização

---
+++ Aula 5 - Thiago/Lista de Exercícios.py
@@ -0,0 +1,62 @@
+# Questão 1:
+# a. Realize o download dos dados de forma manual e crie uma lista e uma tupla com as
+# informações disponíveis no documento csv (coloque pelo menos 1 linha por estado e 10
+# regiões de saúde diferentes, algo próximo de umas 40 linhas).
+
+
+
+RioGrandedoNorte = ['R1 - São José do Mipibu', 
+                    'R2 - Mossoró',
+                    'R3 - João Câmara',
+                    'R4 - Caicó',
+                    'R5 - Santa Cruz',
+                    'R6 - Pau dos Ferros',
+                    'R7 - Metropolitana',
+                    'R8 - Assu']
+
+print (RioGrandedoNorte)
+
+
+
+
+Paraíba
+R1
+R2
+R3
+R4
+R5
+R6
+R7
+R8
+R9
+R10
+R11
+R12
+
+
+Rio de Janeiro
+Baia da Ilha Grande, 
+Baixada Litorânea, 
+Centro-Sul, 
+Médio Paraíba, 
+Metropolitana I, 
+Metropolitana II, 
+Noroeste, Norte
+Serrana.
+
+
+Rio Grande do Sul
+    R1 - Verdes Campos
+    R2 - Entre Rios 
+    R3 - Fronteira Oeste
+    R4 - Belas Praias 
+    R5 - Bons Ventos
+    R6 - Vale do Paranhana Costa da Serra 
+    R7 - Vale dos Sinos
+    R8 - Vale do Caí/Metropolitana
+    R9 - Cabonífera/ Costa Doce
+    R10 - Capital/ Vale do Gravataí
+
+Lista = []
+
+
