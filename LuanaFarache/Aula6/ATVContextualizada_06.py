b8e8871e759f6ef1dc0041b4e6e391b7afad8812
LuanaFarache <89166243+LuanaFarache@users.noreply.github.com>
2021-10-08 02:35:46 -0300

    Atualização

--- LuanaFarache/Aula6/ATVContextualizada_06.py
+++ LuanaFarache/Aula6/ATVContextualizada_06.py
@@ -17,7 +17,7 @@ e.	O programa deve conter pelo menos 1 variável de cada tipo (lista, tupla e di
 OpAnimal=('Rato','Camudongo','Coelho')
 Animal=0
 while Animal==0:
-    Animal = int(input('Insira uma valor de 1 a 3 para informar a espécie de animal: \n 1-Rato \n 2-Camudongo \n 3-Coelho \n'))
+    Animal = int(input('Insira uma valor de 1 a 3 para informar a ESPÉCIE de animal: \n 1-Rato \n 2-Camudongo \n 3-Coelho \n'))
     if Animal == 1 or Animal == 2 or Animal == 3:
         print('Você escolheu a opção:',OpAnimal[Animal-1],'\n')
     else:
@@ -27,44 +27,44 @@ while Animal==0:
 OpAnestesia=('Cetamina + Xilazina','Cetamina + Midazolan','Halotano(Gasoso)')
 Anestesia=0
 while Anestesia==0:
-    Anestesia = int(input('Insira uma valor de 1 a 3 para informar os fármacos anestésicos: \n 1-Cetamina + Xilazina \n 2-Cetamina + Midazolan \n 3-Halotano(Gasoso) \n'))
+    Anestesia = int(input('Insira uma valor de 1 a 3 para informar os fármacos ANESTÉSICOS: \n 1-Cetamina + Xilazina \n 2-Cetamina + Midazolan \n 3-Halotano(Gasoso) \n'))
     if Anestesia == 1 or Anestesia == 2 or Anestesia == 3:
         print('Você escolheu a opção:',OpAnestesia[Anestesia-1],'\n')
     else:
         print('Digite uma informação válida!! \n')
         Anestesia=0
 
-Peso = int(input('Insira o valor do peso do animal em kg: \n'))
+Peso = int(input('Insira o valor do PESO do animal em kg: \n'))
 
 if Anestesia == 1:
     if Animal == 1 or Animal == 2:
         Cetamina=80*Peso
         Xilazina=15*Peso
-        print('Para anestesiar o animal, ministre',Cetamina, 'mg de Cetamina e',Xilazina,' mg de Xilazina \n')
+        print('Para anestesiar o animal, ministre',Cetamina, 'mg de Cetamina e',Xilazina,'mg de Xilazina \n')
     else: 
         Cetamina=50*Peso
         Xilazina=10*Peso
-        print('Para anestesiar o animal, ministre',Cetamina, 'mg de Cetamina e',Xilazina,' mg de Xilazina \n')
+        print('Para anestesiar o animal, ministre',Cetamina, 'mg de Cetamina e',Xilazina,'mg de Xilazina \n')
 elif Anestesia == 2:
     if Animal == 1 or Animal == 2:
         Cetamina=80*Peso
         Midazolan=5*Peso
-        print('Para anestesiar o animal, ministre',Cetamina, 'mg de Cetamina e',Midazolan,' mg de Midazolan \n')
+        print('Para anestesiar o animal, ministre',Cetamina, 'mg de Cetamina e',Midazolan,'mg de Midazolan \n')
     else:
         Cetamina=50*Peso
         Midazolan=2*Peso
-        print('Para anestesiar o animal, ministre',Cetamina, 'mg de Cetamina e',Midazolan,' mg de Midazolan \n')   
+        print('Para anestesiar o animal, ministre',Cetamina, 'mg de Cetamina e',Midazolan,'mg de Midazolan \n')   
 else:
-    print('Para anestesiar o animal, realize a indução com agente inalatório Halotano em até 5% e Manutenção entre 1 a 3% \n')
+    print('Para anestesiar o animal, realize a Indução com agente inalatório Halotano em até 5% e Manutenção entre 1 a 3% \n')
 
-print('Realize o POSICIONAMENTO do animal no estereotáxico. Verifique a angulação da cabeça do animal, a qual deve estar sem diferenças de angulação entre o bregma e o lambda, para ter uma superfície de cirurgia plana. \n')
+print('Após a anestesia, realize o POSICIONAMENTO do animal no estereotáxico. Verifique a angulação da cabeça do animal, a qual deve estar sem diferenças de angulação entre o bregma e o lambda, para ter uma superfície de cirurgia plana. \n')
 Posicionamento=0
 while Posicionamento==0:
-    Posicionamento = int(input('O posicionamento do Animal foi realizado adequadamente? Insira 0 ou 1 para: \n 0 - Animal BEM Posicionado \n 1 - Animal MAL Posicionado \n'))
+    Posicionamento = int(input('O posicionamento do Animal foi realizado adequadamente? Insira 0 ou 1 para: \n 0 - Animal MAL Posicionado \n 1 - Animal BEM Posicionado \n'))
     if Posicionamento == 1:
         print('Você escolheu realizou o posicionamento do animal adequadamente! \n')
     elif Posicionamento == 0:
-        print('Ajuste o posicionamento do Animal... \n')
+        print('Corrija o posicionamento do Animal... \n')
     else:
         print('Digite uma informação válida!! \n')
         Posicionamento=0
@@ -72,45 +72,101 @@ while Posicionamento==0:
 print('Realize a LIMPEZA do campo de trabalho. \n')
 Limpeza=0
 while Limpeza==0:
-    Limpeza = int(input('A LIMPEZA do campo de trabalho foi realiza? Insira 0 ou 1 para: \n 0 - Não \n 1 - Sim \n'))
+    Limpeza = int(input('A LIMPEZA do campo de trabalho foi realizada? Insira 0 ou 1 para: \n 0 - Não \n 1 - Sim \n'))
     if Limpeza == 1:
-        print('Você escolheu realizou a LIMPEZA! \n')
+        print('Você realizou a LIMPEZA do campo de trabalho! \n')
     elif Limpeza == 0:
         print('Faça a LIMPEZA do campo de trabalho... \n')
     else:
         print('Digite uma informação válida!! \n')
         Limpeza=0
 
-# I.	Procedimento de anestesia: Pode-se utilizar uma diversidade de fármacos para anestesia os animais, dentre eles Ketamina e xilazina utilizados em conjunto, halotano (gasoso). Verificar a dosagem correta de acordo com o peso dos animais.
-# II.	Depois do anestésico ter feito efeito, deve-se posicionar o animal no estereotáxico. As barras que suportam o peso do animal devem ser posicionadas no ouvido externo do animal. Normalmente o animal dá uma pequena piscada, devido ao estímulo da musculatura responsável por este movimento. Em seguida verificar a angulação da cabeça do animal, a qual deve estar sem diferenças de angulação entre o bregma e o lambda, para ter uma superfície de cirurgia plana.
-# III.	Limpeza do campo de trabalho: Este procedimento requer o cumprimento de algumas etapas: Retirada da pelagem que recobre a parte superior da calota craniana, Retirada dos tecidos moles (epiderme, derme e tecido conjuntivo) até alcançar a parte óssea da caixa craniana. Por último e não menos importante deve-se limpar a calota craniana de qualquer resto de “pele” que esteja sobrando utilizando H2O2 10 volumes.
-# IV.	Com o animal em posição e com o campo cirúrgico devidamente limpo, utiliza-se uma pequena camada de poliacrilato em todo o perímetro externo para evitar sangramentos.
-# V.	Após este procedimento deve-se escolher um ponto para a fixação de parafusos, de preferência na parte posterior da calota craniana, pois a camada óssea é mais espessa e suporta uma maior profundidade do parafuso. 
-# Obs: Cuidar para não aprofundar muito o parafuso. Com parafusos maiores deve-se dar até 3 voltas no parafuso.
-# Posicionar a agulha (devidamente preparada para o tamanho da cânula e que servirá de suporte para a fixação das cânulas) sobre o bregma (ver figura acima). Fazer os cálculos de posicionamento AnteroPosterior (AP), LateroLateral (LL) e DorsoVentral (DV). Os valores utilizados para os cálculos são os valores encontrados nas réguas a partir do posicionamento da agulha.
-# E. G.: Hipocampo (CA1): Valores hipotéticos para fins de cálculo.
-# AP: 6,42 cm 
-# LL: 3,23 cm
-# DV: 4,2 cm
-# Isso significa que estes foram os valores encontrados em cada régua do estereotáxico e se subtrai ou soma a estes, os valores de cada estrutura ou núcleo. CA1: AP - 0,42 cm LL +/- 0,30 cm DV - 0,20 cm.
-# AP: 6,42 cm – 0,42 = 6,00
-# LL: 3,33 cm + 0,30 = 3,63 / - 0,30 = 3,03  (BILATERAL)
-# DV: 4,20 cm – 0,20 = 4,00
+print('Aplique uma pequena camada de poliacrilato. \n')
+CamadaP=0
+while CamadaP==0:
+    CamadaP = int(input('A aplicação da CAMADA de poliacrilato foi realizada? Insira 0 ou 1 para: \n 0 - Não \n 1 - Sim \n'))
+    if CamadaP == 1:
+        print('Você realizou a aplicação da CAMADA de poliacrilato! \n')
+    elif CamadaP == 0:
+        print('Faça a aplicação da CAMADA de poliacrilato... \n')
+    else:
+        print('Digite uma informação válida!! \n')
+        CamadaP=0
 
-# VI.	Após estes cálculos feitos é hora de localizar os pontos de inserção das cânulas-guia. Assim que estes pontos forem localizados é necessário fazer furos para a introdução das cânulas-guia. A localização destes pontos deve ser definida pelos valores encontrados nos cálculos AnteroPosterior (6,00) e LateroLateral (3,63 e 3,03). Deve-se escolher qual dos hemisférios vai ser colocada a primeira cânula, daí os dois valores para as medidas LL.
-# VII.	Depois de posicionar a agulha, fazer um furo com a broca até alcançar as meninges. A não perfuração das meninges é o procedimento ideal, e para conseguir isso apoie a mão que segura a broca contra o assoalho ou ao estereotáxico e perfure o crânio a +- 450 de angulação.
-# VIII.	Após ter atingido este objetivo, introduza a cânula-guia previamente confeccionada até o valor DorsoVentral (4,00) que foi calculado anteriormente.
-# IX.	Logo após drenar qualquer sangue ou líquido cefalorraquidiano que esteja saindo pelo orifício criado no crânio. Para isso utilize pequenos rolos de papel absorvente.
-# X.	Faça uma mistura do acrílico polimerizante com o solvente até ficar com textura espessa porém maleável (o ideal é que a mistura seja capaz de cobrir a parte desejada sem escorrer por todo o crânio). Com essa mistura faça um capacete abrangendo o crânio, a cânula-guia e o parafuso. Deixe secar até ficar suficientemente rígido. O tempo de secagem varia de acordo com a temperatura e umidade da sala. 
-# XI.	O próximo passo é a fixação da outra cânula-guia. Deve-se levantar levemente o braço do estereotáxico cuidando para que a cânula-guia previamente fixada não se movimente. Logo após, posicionar a agulha sobre o outro ponto de inserção da cânula-guia. Introduzir a cânula-guia até o valor DV (4,00) calculado previamente.
-# XII.	Seguir novamente a descrição do item 9 e após fixar a cânula conforme item 10. De preferência espalhar o cimento sobre a maior área do crânio, sempre deixando um espaço entre o capacete e o início da área tecidual. Este cuidado previne de um futuro descolamento do capacete devido a entrada de sangue u outro líquido entre o capacete e o crânio.
-# XIII.	Levantar bem devagar seguindo as instruções do item contida no item 11. Acomodar o animal em uma caixa aquecida por uma lâmpada e sem outros animais acordados. Assim que o animal despertar colocá-lo de volta a sua caixa-moradia.
+print('Determine os pontos para FIXAÇÃO dos parafusos e realize a FIXAÇÃO. \n')
+FixaçãoPf=0
+while FixaçãoPf==0:
+    FixaçãoPf = int(input('A FIXAÇÃO dos parafusos foi realizada? Insira 0 ou 1 para: \n 0 - Não \n 1 - Sim \n'))
+    if FixaçãoPf == 1:
+        print('Você realizou a FIXAÇÃO dos parafusos! \n')
+    elif FixaçãoPf == 0:
+        print('Faça a FIXAÇÃO dos parafusos... \n')
+    else:
+        print('Digite uma informação válida!! \n')
+        FixaçãoPf=0
 
+AjusteCoordenadas={}
+AjusteCoordenadas['Ajustes'] = {'AP' : 0.42, 'LL' : 0.30, 'DV' : 0.20}
+CoordenadasEs={}
+CoordenadasEs['HipoCA1'] = {'AP' : float(input('Insira a medida AnteroPosterior (AP) em cm:')), 'LL' : float(input('Insira a medida LateroLateral (LL) em cm:')),'DV' : float(input('Insira a medida DorsoVentral (DV) em cm:'))}
+# #CoordenadasEs['Amígdala'] = {'AP' : float(input('Insira a medida AnteroPosterior (AP) em cm:')), 'LL' : float(input('Insira a medida LateroLateral (LL) em cm:')),'DV' : float(input('Insira a medida DorsoVentral (DV) em cm:'))}
+# #CoordenadasEs['CortexMotor'] = {'AP' : float(input('Insira a medida AnteroPosterior (AP) em cm:')), 'LL' : float(input('Insira a medida LateroLateral (LL) em cm:')),'DV' : float(input('Insira a medida DorsoVentral (DV) em cm:'))}
 
+AP = CoordenadasEs['HipoCA1'].get('AP')-AjusteCoordenadas['Ajustes'].get('AP')
+LL1 = CoordenadasEs['HipoCA1'].get('LL')-AjusteCoordenadas['Ajustes'].get('LL')
+LL2 = CoordenadasEs['HipoCA1'].get('LL')+AjusteCoordenadas['Ajustes'].get('LL')
+DV = CoordenadasEs['HipoCA1'].get('DV')-AjusteCoordenadas['Ajustes'].get('DV')
 
+print('Valores calculados são:\n AP=',AP,'LL1=',LL1,'LL2=',LL2,'DV=',DV,'\n')
 
+print('Localize os pontos de INSERÇÃO das cânulas-guia, faça os furos. \n')
+Inserção=0
+while Inserção==0:
+    Inserção = int(input('A INSERÇÃO das cânulas-guia foi realizada? Insira 0 ou 1 para: \n 0 - Não \n 1 - Sim \n'))
+    if Inserção == 1:
+        print('Você realizou a INSERÇÃO das cânulas-guia! \n')
+    elif Inserção == 0:
+        print('Faça a INSERÇÃO das cânulas-guia... \n')
+    else:
+        print('Digite uma informação válida!! \n')
+        Inserção=0
 
+Escolha=0
+while Escolha==0:
+    Escolha = int(input('Escolha o Hemisfério do LL. Insira 0 ou 1 para: \n 1 - Direito \n 2 - Esquerdo \n'))
+    if Escolha == 1:
+        print('Você escolheu o hemisfério Direito! \n')
+    elif Escolha == 2:
+        print('Você escolheu o hemisfério esquerdo \n')
+    else:
+        print('Digite uma informação válida!! \n')
+        Escolha=0
 
+Ângulo=0
+while Ângulo==0:
+    Ângulo = int(input('O ÂNGULO de perfuração está adequado? Insira 0 ou 1 para: \n 0 - Não \n 1 - Sim \n'))
+    if Ângulo == 1:
+        print('Você ajustou o ângulo adequadamente! \n')
+    elif Ângulo == 0:
+        print('Ajuste o ângulo... \n')
+    else:
+        print('Digite uma informação válida!! \n')
+        Ângulo=0
+
+Final=0
+while Final==0:
+    Final = int(input('Você realizou as etapas de drenagem e de instalação da segunda cânula? Insira 0 ou 1 para: \n 0 - Não \n 1 - Sim \n'))
+    if Final == 1:
+        print('Finalize a cirurgia e deixe o animal em repouso isolado! \n')
+    elif Final == 0:
+        print('Realize as etapas de drenagem e de instalação da segunda cânula... \n')
+    else:
+        print('Digite uma informação válida!! \n')
+        Final=0
+
+print('EXPERIMENTO FINALIZADO!')
+
+#REFERÊNCIA: https://www.ufmg.br/bioetica/cetea/index2.php?option=com_content&do_pdf=1&id=22#:~:text=c)%20Rotule%20com%20a%20informa%C3%A7%C3%A3o,a%20%C2%BD%20da%20dose%20inicial.
 
 
 
