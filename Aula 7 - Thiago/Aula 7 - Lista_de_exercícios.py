a0407d1b21176207d6f703913e9549d7058d229e
ftthiagoaraujo <88913003+ftthiagoaraujo@users.noreply.github.com>
2021-10-17 19:26:11 -0300

    .

--- Aula 7 - Thiago/Aula 7 - Lista_de_exercícios.py
+++ Aula 7 - Thiago/Aula 7 - Lista_de_exercícios.py
@@ -1,8 +1,61 @@
-def dispositivo_optogenética(entradas para a função)
+# 32 Entradas: Matriz de Eletrodos com 32 variáveis reais:
+_01ªvariável = 1
+_02ªvariável = 0
+_03ªvariável = 2
+_04ªvariável = 0
+_05ªvariável = 1
+_06ªvariável = 0
+_07ªvariável = 1
+_08ªvariável = 0
+_09ªvariável = 1
+_10ªvariável = 0
+_11ªvariável = 2
+_12ªvariável = 0
+_13ªvariável = 1
+_14ªvariável = 0
+_15ªvariável = 2
+_16ªvariável = 0
+_17ªvariável = 0
+_18ªvariável = 1
+_19ªvariável = 1
+_20ªvariável = 1
+_21ªvariável = 0
+_22ªvariável = 0
+_23ªvariável = 0
+_24ªvariável = 0
+_25ªvariável = 0
+_26ªvariável = 0
+_27ªvariável = 1
+_28ªvariável = 2
+_29ªvariável = 1
+_30ªvariável = 0
+_31ªvariável = 0
+_32ªvariável = 0
+
+#Matriz_de_Eletrodos = (_01ªvariável + _02ªvariável +  _03ªvariável + _04ªvariável +  _05ªvariável + _06ªvariável + _07ªvariável + _08ªvariável + _09ªvariável + _10ªvariável +  _11ªvariável + _12ªvariável + _13ªvariável + _14ªvariável + _15ªvariável + _16ªvariável + _17ªvariável + _18ªvariável + _19ªvariável + _20ªvariável + _21ªvariável + _22ªvariável + _23ªvariável + _24ªvariável + _25ªvariável + _26ªvariável + _27ªvariável + _28ªvariável + _29ªvariável + _30ªvariável + _31ªvariável + _32ªvariável)
+#print(Matriz_de_Eletrodos)
+
+def Matriz_de_Eletrodos(_01ªvariável,_02ªvariável,_03ªvariável,_04ªvariável,_05ªvariável,_06ªvariável,_07ªvariável,_08ªvariável,_09ªvariável,_10ªvariável,_11ªvariável,_12ªvariável,_13ªvariável,_14ªvariável,_15ªvariável,_16ªvariável,_17ªvariável,_18ªvariável,_19ªvariável,_20ªvariável,_21ªvariável,_22ªvariável,_23ªvariável,_24ªvariável,_25ªvariável,_26ªvariável,_27ªvariável,_28ªvariável,_29ªvariável,_30ªvariável,_31ªvariável,_32ªvariável):
+"""O que colocar aqui???"""
+    return _01ªvariável + _02ªvariável +  _03ªvariável + _04ªvariável +  _05ªvariável + _06ªvariável + _07ªvariável + _08ªvariável + _09ªvariável + _10ªvariável +  _11ªvariável + _12ªvariável + _13ªvariável + _14ªvariável + _15ªvariável + _16ªvariável + _17ªvariável + _18ªvariável + _19ªvariável + _20ªvariável + _21ªvariável + _22ªvariável + _23ªvariável + _24ªvariável + _25ªvariável + _26ªvariável + _27ªvariável + _28ªvariável + _29ªvariável + _30ªvariável + _31ªvariável + _32ªvariável
+
+Intensidade_LED_Vermelho = str(input("Qual a intensidade do LED Vermelho:\n")
+Intensidade_LED_Verde = input("Qual a intensidade do LED Verde:\n")
+Intensidade_LED_Azul = input("Qual a intensidade do LED Azul:\n")
+print(add(Intensidade_LED_Vermelho,Intensidade_LED_Verde,Intensidade_LED_Azul))
+print("Fim de Programa")
 
 
 
+# André disse que um dicinionairio pode caber bem pra organizar essas infroamções(so segundo ponto)
+# 1,2 ou 0 voltz de tensão no terminal do led
+# tensao ou não tensão
+# dependendo de uma intensidade, vai botar a tensão pra luz vermelha
+# faz a mesma coisa para os outros terminais.
+# o 4º terminal é o terra. pra onde a corrente tem quesair depois queentra nos outros terminais.
 
-led RGB para excitação (saída)
-
-# printar o resultado de decisão é considerado saída.
+# 3 Saídas: LED RGB e suas 3 variáveis
+# esse led vai estimular o cerebro para mudar o padrão de tensão, que vai ser medido pelos eletrodos.
+# Intensidade_LED_Vermelho = input("Qual a intensidade do LED Vermelho:\n")
+# Intensidade_LED_Verde = int(input("Qual a intensidade do LED Verde:"))
+# Intensidade_LED_Azul = int(input("Qual a intensidade do LED Azul:"))
