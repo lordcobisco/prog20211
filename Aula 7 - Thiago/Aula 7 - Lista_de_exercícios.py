47d4ae745f63bb924ee1bb77857b9631827b968e
ftthiagoaraujo <88913003+ftthiagoaraujo@users.noreply.github.com>
2021-10-18 09:23:46 -0300

    Update Aula 7 - Lista_de_exercícios.py

--- Aula 7 - Thiago/Aula 7 - Lista_de_exercícios.py
+++ Aula 7 - Thiago/Aula 7 - Lista_de_exercícios.py
@@ -1,4 +1,3 @@
-# 32 Entradas: Matriz de Eletrodos com 32 variáveis reais:
 _01ªvariável = 1
 _02ªvariável = 0
 _03ªvariável = 2
@@ -32,19 +31,16 @@ _30ªvariável = 0
 _31ªvariável = 0
 _32ªvariável = 0
 
-#Matriz_de_Eletrodos = (_01ªvariável + _02ªvariável +  _03ªvariável + _04ªvariável +  _05ªvariável + _06ªvariável + _07ªvariável + _08ªvariável + _09ªvariável + _10ªvariável +  _11ªvariável + _12ªvariável + _13ªvariável + _14ªvariável + _15ªvariável + _16ªvariável + _17ªvariável + _18ªvariável + _19ªvariável + _20ªvariável + _21ªvariável + _22ªvariável + _23ªvariável + _24ªvariável + _25ªvariável + _26ªvariável + _27ªvariável + _28ªvariável + _29ªvariável + _30ªvariável + _31ªvariável + _32ªvariável)
-#print(Matriz_de_Eletrodos)
-
 def Matriz_de_Eletrodos(_01ªvariável,_02ªvariável,_03ªvariável,_04ªvariável,_05ªvariável,_06ªvariável,_07ªvariável,_08ªvariável,_09ªvariável,_10ªvariável,_11ªvariável,_12ªvariável,_13ªvariável,_14ªvariável,_15ªvariável,_16ªvariável,_17ªvariável,_18ªvariável,_19ªvariável,_20ªvariável,_21ªvariável,_22ªvariável,_23ªvariável,_24ªvariável,_25ªvariável,_26ªvariável,_27ªvariável,_28ªvariável,_29ªvariável,_30ªvariável,_31ªvariável,_32ªvariável):
-"""O que colocar aqui???"""
+    """O que colocar aqui???"""
     return _01ªvariável + _02ªvariável +  _03ªvariável + _04ªvariável +  _05ªvariável + _06ªvariável + _07ªvariável + _08ªvariável + _09ªvariável + _10ªvariável +  _11ªvariável + _12ªvariável + _13ªvariável + _14ªvariável + _15ªvariável + _16ªvariável + _17ªvariável + _18ªvariável + _19ªvariável + _20ªvariável + _21ªvariável + _22ªvariável + _23ªvariável + _24ªvariável + _25ªvariável + _26ªvariável + _27ªvariável + _28ªvariável + _29ªvariável + _30ªvariável + _31ªvariável + _32ªvariável
 
-Intensidade_LED_Vermelho = str(input("Qual a intensidade do LED Vermelho:\n")
-Intensidade_LED_Verde = input("Qual a intensidade do LED Verde:\n")
-Intensidade_LED_Azul = input("Qual a intensidade do LED Azul:\n")
-print(add(Intensidade_LED_Vermelho,Intensidade_LED_Verde,Intensidade_LED_Azul))
-print("Fim de Programa")
-
+Intensidade_LED_Vermelho = input(str("Qual a intensidade do LED Vermelho:\n"))
+Intensidade_LED_Verde = input(str("Qual a intensidade do LED Verde:\n"))
+Intensidade_LED_Azul = input(str("Qual a intensidade do LED Azul:\n"))
+print(Intensidade_LED_Vermelho)
+print(Intensidade_LED_Verde)
+print(Intensidade_LED_Azul)
 
 
 # André disse que um dicinionairio pode caber bem pra organizar essas infroamções(so segundo ponto)
